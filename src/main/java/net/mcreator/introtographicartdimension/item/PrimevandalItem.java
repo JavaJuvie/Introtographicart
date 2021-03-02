1db7bc01ee008a19215a42fffd7ad181281a8d3e
Madpops52 <mathi@10.0.0.29>
2021-03-01 21:22:18 -0500

    - added prime classic - fixed prime vandal use animation

--- src/main/java/net/mcreator/introtographicartdimension/item/PrimevandalItem.java
+++ src/main/java/net/mcreator/introtographicartdimension/item/PrimevandalItem.java
@@ -104,7 +104,7 @@ public class PrimevandalItem extends IntroToGraphicArtDimensionModElements.ModEl
 						}
 					}
 					if (entity.abilities.isCreativeMode || stack != ItemStack.EMPTY) {
-						ArrowCustomEntity entityarrow = shoot(world, entity, random, 10f, 20, 10);
+						ArrowCustomEntity entityarrow = shoot(world, entity, random, 1f, 20, 10);
 						itemstack.damageItem(1, entity, e -> e.sendBreakAnimation(entity.getActiveHand()));
 						if (entity.abilities.isCreativeMode) {
 							entityarrow.pickupStatus = AbstractArrowEntity.PickupStatus.CREATIVE_ONLY;
@@ -203,7 +203,7 @@ public class PrimevandalItem extends IntroToGraphicArtDimensionModElements.ModEl
 		double d0 = target.getPosY() + (double) target.getEyeHeight() - 1.1;
 		double d1 = target.getPosX() - entity.getPosX();
 		double d3 = target.getPosZ() - entity.getPosZ();
-		entityarrow.shoot(d1, d0 - entityarrow.getPosY() + (double) MathHelper.sqrt(d1 * d1 + d3 * d3) * 0.2F, d3, 10f * 2, 12.0F);
+		entityarrow.shoot(d1, d0 - entityarrow.getPosY() + (double) MathHelper.sqrt(d1 * d1 + d3 * d3) * 0.2F, d3, 1f * 2, 12.0F);
 		entityarrow.setSilent(true);
 		entityarrow.setDamage(20);
 		entityarrow.setKnockbackStrength(10);
